20da154de29dcfb8225eefa3f8c9df5098a9abcc
Jose Silvio <josesilvio.bs@gmail.com>
2023-04-10 15:15:58 -0300

    Fazendo alteracao no index.js

--- api-livraria-router/index.js
+++ api-livraria-router/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const routers = require('./routers/index.js');
-const db = require('./db_ config.js')
 const app = express();
 const port = process.env.PORT || 3000;
 routers(app)
 
-db.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`));
 app.get('/',(req, res) => {
     res.send("ola projeto node")
 })
+
 app.listen(port, () => {
     console.log(`Servidor ativo na porta ${port}`)
 })
