6b50e211b3fa0ccfbb764d3e4b6df6b0ff54fa21
Jose Silvio <josesilvio.bs@gmail.com>
2023-04-10 14:50:02 -0300

    Fazendo algumas alterações no index e no package json

--- api-livraria-router/index.js
+++ api-livraria-router/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routers = require('./routers/index.js');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 routers(app)
 app.listen(port, () => {
     console.log(`Servidor ativo na porta ${port}`)
