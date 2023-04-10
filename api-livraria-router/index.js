1ff16f7566fb656de082f609f2edd9569e8ae0d9
Jose Silvio <josesilvio.bs@gmail.com>
2023-04-10 14:53:20 -0300

    Fazendo alteracao no index.js

--- api-livraria-router/index.js
+++ api-livraria-router/index.js
@@ -3,6 +3,9 @@ const routers = require('./routers/index.js');
 const app = express();
 const port = process.env.PORT || 3000;
 routers(app)
+app.get('/',(req, res) => {
+    res.send("ola projeto node")
+})
 app.listen(port, () => {
     console.log(`Servidor ativo na porta ${port}`)
 })
